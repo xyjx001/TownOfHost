428916f8cd19485b2d7fa1179cdda333fa44e52e
shu-TownofHost <itozakishunnya@gmail.com>
2022-06-09 16:26:05 +0900

    遅延を0.4秒に変更

--- Patches/MeetingHudPatch.cs
+++ Patches/MeetingHudPatch.cs
@@ -202,7 +202,7 @@ namespace TownOfHost
                     new LateTask(() =>
                     {
                         pc.RpcRevertShapeshift(false);//ミミックの変身したままで会議に突入し、終わった時に変身が解除されないため、会議開始時に全員のシェイプを解除
-                    }, 0.2f, "AllRevertShapeshift");
+                    }, 0.4f, "AllRevertShapeshift");
             }
         }
         public static void Postfix(MeetingHud __instance)
