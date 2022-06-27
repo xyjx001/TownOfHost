382435a43ce8161b1fdb5d50a11e8045b59c76bd
EmptyBottle <ttes3511@hotmail.com>
2022-06-27 18:56:09 +0900

    CustomRoleManagerクラスを追加

---
+++ Roles/CustomRoleManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Hazel;
+using UnityEngine;
+
+namespace TownOfHost
+{
+    public class CustomRoleManager
+    {
+        public static List<RoleBase> RoleInstances;
+        public static void InitAllInstance()
+        {
+        }
+    }
+}
